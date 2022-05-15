61039b29ace75643620796b768040569f5905f8b
nourHalhoul <nourhalhoul@gmail.com>
2022-05-15 19:56:17 -0400

    Built aboutme page framework

--- src/App.js
+++ src/App.js
@@ -1,24 +1,41 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import ProfilePic from './ProfilePic.jpg';
+import { useMediaQuery } from 'react-responsive'
 
-function App() {
+
+function App()
+{
+  const isTabletOrMobile = useMediaQuery({ maxWidth: 600 })
+  const isDesktopOrLaptop = useMediaQuery({ minWidth: 601 })
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <div className='homepage'>
+      <img src={ProfilePic} alt='Profile' className='profilepic'></img>
+      <div className='layout'>
+        {isTabletOrMobile && <div className='badges'>
+          <i class="devicon-javascript-plain colored" title="JavaScript"></i><i class="devicon-csharp-plain colored" title="C#"></i><i class="devicon-html5-plain colored" title="HTML"></i><i class="devicon-css3-plain colored" title="CSS"></i><i class="devicon-sqlite-plain colored" title="SQL"></i>
+        </div>} {/*Mobile*/}
+        {isDesktopOrLaptop && <div className='badges'>
+          <i class="devicon-javascript-plain colored" title="JavaScript"></i>&nbsp;<i class="devicon-csharp-plain colored" title="C#"></i>&nbsp;<i class="devicon-html5-plain colored" title="HTML"></i>&nbsp;<i class="devicon-css3-plain colored" title="CSS"></i>&nbsp;<i class="devicon-sqlite-plain colored" title="SQL"></i>
+        </div>} {/*Desktop*/}
+        <div className='name'>
+          <h1>Nour H.</h1>
+        </div>
+        <div className='aboutme'>
+          <p>About me.. About me..<br /> About me..<br /> About me..<br /> About me.. About me.. About me.. About me.. About me.. About me.. </p>
+        </div>
+        <div className='languages'>
+          <p> Languages: JavaScript | C# | HTML | CSS | SQL
+          </p>
+        </div>
+        <div className='buttons'>
+          <button className='aboutbutton'>About me</button>
+          <button className='blogbutton'>Blog</button>
+          <button className='contactbutton'>Contact</button>
+        </div>
+      </div>
+    </div >
   );
 }
 
